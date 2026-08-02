cc3f996d44354bb6864526fc83bfc1a002830a17
Prajwal dhakal <147418042+SillyLix@users.noreply.github.com>
2026-08-02 15:53:09 +0300

    part 3d Deploying the database backend to production

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,8 @@ const errorHandler = (error, req, res, next) => {
 
 	if (error.name === 'CastError') {
 		return res.status(400).send({ error: 'malformatted  id' });
+	} else if (error.name === 'ValidationError') {
+		return res.status(400).json({ error: error.message });
 	}
 
 	next(error);
@@ -49,7 +51,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
 		.catch((error) => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 	const body = request.body;
 
 	if (!body.content) {
@@ -66,9 +68,12 @@ app.post('/api/notes', (request, response) => {
 
 	console.log(Date.now());
 
-	note.save().then((savedNote) => {
-		response.json(savedNote);
-	});
+	note
+		.save()
+		.then((savedNote) => {
+			response.json(savedNote);
+		})
+		.catch((error) => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
